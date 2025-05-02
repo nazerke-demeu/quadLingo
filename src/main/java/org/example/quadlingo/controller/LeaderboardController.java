80fb79cff776e05b8912d019c90206208fee95e6
Saida <saida.sagintaeva30@gmail.com>
2025-05-02 23:14:27 +0500

    Added leaderboard functionality

---
+++ src/main/java/org/example/quadlingo/controller/LeaderboardController.java
@@ -0,0 +1,27 @@
+package org.example.quadlingo.controller;
+
+import org.example.quadlingo.GameState;
+import org.example.quadlingo.repository.GameStateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/leaderboard")
+public class LeaderboardController {
+
+    @Autowired
+    private GameStateRepository gameStateRepository;
+
+    @GetMapping
+    public String leaderboard(Model model) {
+        List<GameState> topPlayers = gameStateRepository.findTop10ByOrderByScoreDesc();
+        model.addAttribute("topPlayers", topPlayers);
+        return "leaderboard";
+    }
+}
+
